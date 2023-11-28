1c2c16f682d016a5bfd47889a2ee169c84431ec8
BruceHill <brucehillwalley@gmail.com>
2023-11-29 00:45:04 +0300

    finished

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,11 +1,12 @@
 
-import { DataMap } from "./ShowData";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Image Gallery</h1>
-      <div>{DataMap}</div>
-    </div>
-  );
-}
+// import { DataMap } from "./ShowData";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Image Gallery</h1>
+//       <div>{DataMap}</div>
+//     </div>
+//   );
+// }
--- react_app/src/ShowData.js
+++ react_app/src/ShowData.js
@@ -1,31 +1,42 @@
 import data from "./data.js";
 import "./index.css";
+import * as emoji from 'node-emoji';
 
 export const DataMap = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: "1rem",
-        flexWrap: "wrap",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+    <>
+   
+    <h1>{emoji.random().emoji} Bruce Image Gallery <sup style={{ color: "#6F8AB4",fontSize:"1rem" }}>with node-emoji</sup>{emoji.random().emoji}</h1>
+  
+    <div className="pictures"
+    //   style={{
+    //     display: "flex",
+    //     gap: "1rem",
+    //     flexWrap: "wrap",
+    //     alignItems: "center",
+    //     justifyContent: "center",
+    //   }}
     >
       {data.map((item) => (
         <>
-          <div className="imageCap">
+        <div className="picture">
+          <div className="imageContainer">
             <img
-              className="image1"
-              style={{ width: "250px", height: "250px", borderRadius: "20px" }}
+              
+            //   style={{ width: "250px", height: "250px", borderRadius: "20px" }}
               src={item.src.large}
               alt=""
             />
           </div>
-
-          <p style={{ color: "#6F8AB4" }}>{item.photographer}</p>
+<div className="info" >
+   <p >{emoji.random().emoji} {item.photographer} {emoji.random().emoji}</p> 
+   
+</div>
+          
+          </div>
         </>
       ))}
     </div>
+    </>
   );
 };
